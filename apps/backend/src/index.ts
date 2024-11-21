ec7a87fc403e379341050af5fab0848941470e3c
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2024-11-21 18:48:46 +0000

    add temperature reading generation

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,6 +4,35 @@ import cors from "cors";
 const app = express();
 const PORT = 3000;
 
+interface TemperatureReading {
+  timestamp: number;
+  temperature: number;
+}
+
+let tempReadings: TemperatureReading[] = [];
+
+const generateRandomTemperature = (): number => {
+  const min = 36.0;
+  const max = 38.0;
+  return Math.random() * (max - min) + min;
+};
+
+const storeTemperatureReadings = () => {
+  let counter = 0;
+
+  const intervalId = setInterval(() => {
+    if (counter < 60 * 5) {
+      const timestamp = Date.now();
+      const temperature = generateRandomTemperature();
+      tempReadings.push({ timestamp, temperature });
+      counter++;
+    } else {
+      clearInterval(intervalId);
+      console.log("Stopped creating temperature readings");
+    }
+  }, 1000);
+};
+
 app.use(express.json());
 app.use(cors());
 
@@ -13,4 +42,5 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  storeTemperatureReadings();
 });
